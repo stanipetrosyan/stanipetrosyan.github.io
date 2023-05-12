d16ba18bc74b031bb06f41dff06bf6c91e02216e
stanipetrosyan <stani.petrosyan@gmail.com>
2023-05-12 14:17:27 +0200

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/stanipetrosyan/",
-  plugins: [svelte()],
-  build: {
-    outDir: './docs'
-  }
+  plugins: [svelte()]
 })
