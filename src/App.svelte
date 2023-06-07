9e3f4b9d1975fcd0ce48cfaab28788b9dd1b25d5
stanipetrosyan <stani.petrosyan@gmail.com>
2023-06-07 14:07:51 +0200

    added some titles

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,28 @@
       <h1 start animation=fadein ease=easeInOutQuad style="--duration: 2s; --delay: 1s;">Welcome, I'm Stanislav</h1>
     </x-cell>
   </x-grid>
+
+  <x-grid columns=8>
+    <x-cell span="3..">
+      <ul class="inline">
+        <button class="is-white">About me</button>
+        <button class="is-blank">Experience</button>
+        <button class="is-blank">Knowledge</button>
+        <button class="is-blank">Certifications</button>
+        <button class="is-blank">Books</button>
+        
+      </ul>
+    </x-cell>
+  </x-grid>
+
+  <h4>Computer enthusiast since
+    first years of high school. I like
+    challenge myself with problems and
+    I dedicate all of myself to them
+    resolution. Working in a world
+    Agile I got the skill to work in
+    Excellent team and management
+    deadlines.</h4>
 </main>
 
 <style>
