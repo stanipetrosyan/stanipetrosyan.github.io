297e578357008f27e8f0e2bdad5777e85afebde9
stanipetrosyan <stani.petrosyan@gmail.com>
2025-05-12 23:09:32 +0200

    feat: fixed home pages

--- src/routes/GameDeveloper.svelte
+++ src/routes/GameDeveloper.svelte
@@ -13,11 +13,11 @@
         tabStyle="pill"
         defaultClass="flex divide-x rtl:divide-x-reverse divide-gray-200 dark:divide-gray-700">
         <TabItem class="w-full" open>
-          <div slot="title" class="text-lg self-center">About</div>
+          <div slot="title" class="text-lg self-center">Ciaoo</div>
           <About />
         </TabItem>
         <TabItem class="w-full">
-          <span slot="title" class="text-lg">Knowledge</span>
+          <span slot="title" class="text-lg">provvissima</span>
           <Knowledge />
         </TabItem>
         <TabItem class="w-full">
@@ -25,9 +25,6 @@
           <Experience />
         </TabItem>
       </Tabs>
-    </div>
-    <div class="place-self-start">
-      <DarkMode />
     </div>
   </div>
 </main>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { Card } from "flowbite-svelte";
+</script>
+
+<span>
+  <div class="grid grid-cols-6 grid-rows-6 gap-4">
+    <div class="col-start-3 col-end-4 row-start-2 row-end-5">
+     <Card class="sm:p-10 md:p-12 p-12">
+        <a href="/">
+          <h5
+            class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white"
+          >
+            Full Stack Developer
+          </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+          Se vuoi conoscere la mia storia lavorativa e conoscenze professionale da Full Stack Developer
+        </p>
+        <a
+          href="/software"
+          class="text-primary-600 inline-flex items-center hover:underline"
+        >
+          See our guideline
+        </a>
+      </Card>
+    </div>
+   <div class="col-start-4 col-end-5 row-start-2 row-end-5">
+      <Card class="sm:p-10 md:p-12 p-12">
+        <a href="/">
+          <h5
+            class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white"
+          >
+          Game Developer
+          </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+          Se vuoi conoscere la mia passione per la creazione dei videogiochi e il mio portfolio
+        </p>
+        <a
+          href="/game"
+          class="text-primary-600 inline-flex items-center hover:underline"
+        >
+          See our guideline
+        </a>
+      </Card>
+    </div>
+  </div>
+</span>
