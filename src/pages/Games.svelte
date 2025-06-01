467d07c59a6ea9396e372a86e590139f99ee7cb7
stanipetrosyan <stani.petrosyan@gmail.com>
2025-06-01 16:53:20 +0200

    added game tab

---
+++ src/pages/Games.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<span> 
+  <p> list of games
+  
+  </p>
+</span>
+
+<style>
+</style>
--- src/routes/GameDeveloper.svelte
+++ src/routes/GameDeveloper.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { Tabs, TabItem, Button, Heading } from "flowbite-svelte";
-  import Knowledge from "../pages/Knowledge.svelte";
-  import Experience from "../pages/Experience.svelte";
+  import Games from "../pages/Games.svelte";
 </script>
 
 <main>
@@ -9,14 +8,11 @@
     <div class="basis-full m-16">
       <Tabs
         tabStyle="pill"
-        defaultClass="flex divide-x rtl:divide-x-reverse divide-gray-200 dark:divide-gray-700">
-        <TabItem class="w-full">
-          <span slot="title" class="text-lg">provvissima</span>
-          <Knowledge />
-        </TabItem>
-        <TabItem class="w-full">
-          <span slot="title" class="text-lg">Experience</span>
-          <Experience />
+        defaultClass="flex divide-x rtl:divide-x-reverse divide-gray-200 dark:divide-gray-700"
+      >
+        <TabItem open class="w-full">
+          <span slot="title" class="text-lg">Games</span>
+          <Games />
         </TabItem>
       </Tabs>
     </div>
