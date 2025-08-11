a4ba5fffe036afa251b0cf24622310fc80a02769
stanipetrosyan <stani.petrosyan@gmail.com>
2025-08-11 22:05:05 +0200

    feat: get started with vuejs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
 })
