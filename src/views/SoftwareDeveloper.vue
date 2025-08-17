5933cf2738fc6cf65e2a814a9f9420450f1e08f8
stanipetrosyan <stani.petrosyan@gmail.com>
2025-08-17 22:48:55 +0200

    feat: refactored software view

--- src/views/SoftwareDeveloper.vue
+++ src/views/SoftwareDeveloper.vue
@@ -1,15 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import { FwbTab, FwbTabs } from 'flowbite-vue'
+import Experience from '@/components/Experience.vue';
+
+const activeTab = ref('first')
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an Softwarer page</h1>
-  </div>
+   <fwb-tabs v-model="activeTab" variant="pills" class="p-5">
+    <fwb-tab name="experience" title="Experience" >
+      <Experience></Experience>
+    </fwb-tab>
+    <fwb-tab name="second" title="Second">
+      Lorem ipsum dolor...
+    </fwb-tab>
+    <fwb-tab name="third" title="Third">
+      Lorem ipsum dolor...
+    </fwb-tab>
+    <fwb-tab name="fourth" title="Fourth" disabled>
+      Lorem ipsum dolor...
+    </fwb-tab>
+  </fwb-tabs>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
