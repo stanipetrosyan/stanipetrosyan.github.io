313ae7dbd36cdd377704716e03949e4685208210
Stanislav Petrosyan <stanislav.petrosyan@thread.solutions>
2025-08-18 13:58:37 +0200

    feat: improved home page

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,62 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { FwbButton, FwbBreadcrumb, FwbBreadcrumbItem } from 'flowbite-vue';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  var themeToggleBtn = document.getElementById('theme-toggle');
+
+  themeToggleBtn.addEventListener('click', function() {
+
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+      }
+
+      // if NOT set via local storage previously
+    } else {
+      if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+      } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+      }
+    }
+  })
+})
+
 </script>
 
 <template>
-  <header>
-    <fwb-breadcrumb>
-      <fwb-breadcrumb-item home href="/home">
-        Home
-      </fwb-breadcrumb-item>
-      <fwb-breadcrumb-item href="/software">
-        Projects
-      </fwb-breadcrumb-item>
-      <fwb-breadcrumb-item>
-        Flowbite
-      </fwb-breadcrumb-item>
-    </fwb-breadcrumb>
-  </header>
+
+  <div class="flex flex-row m-8">
+    <div class="flex justify-end">
+      <fwb-button id="theme-toggle">Dark</fwb-button>
+    </div>
+    <div class="place-self-start">
+      <fwb-breadcrumb>
+        <fwb-breadcrumb-item home href="/">
+          Home
+        </fwb-breadcrumb-item>
+        <fwb-breadcrumb-item href="/software">
+          Projects
+        </fwb-breadcrumb-item>
+        <fwb-breadcrumb-item>
+          Flowbite
+        </fwb-breadcrumb-item>
+      </fwb-breadcrumb>
+    </div>
+
+
+
+  </div>
 
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,66 @@
 <script setup>
+import { FwbImg, FwbHeading, FwbCard, FwbFooter } from 'flowbite-vue'
+
+
 </script>
 
 <template>
   <main>
+    <span class="mt-10">
+      <div class="grid grid-cols-8 grid-rows-2 gap-4 mt-10">
+        <div class="col-start-4 col-end-6 row-start-1 row-end-2 mb-10">
+          <fwb-img src="../assets/myself.jpg" class="h-64 w-64 rounded-full mx-auto mb-8" />
+
+          <fwb-heading tag="h5">I have been passionate about computer science since my early high
+            school years. I like to challenge myself with problems and dedicate
+            myself fully to solving them.</fwb-heading>
+        </div>
+
+        <div class="col-start-2 col-end-5 row-start-2 row-end-3">
+          <fwb-card class="sm:p-10 md:p-12 p-12 w-xl" variant="horizontal">
+            <a href="/software">
+              <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                Full Stack Developer
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              Passionate about computer science, with a strong interest in
+              high-quality software development. I pay particular attention to
+              writing clean, readable, and maintainable code. I see myself as a Full
+              Stack Developer, as I have learned techniques that cover the entire
+              project development lifecycle.
+            </p>
+            <a href="/software" class="text-primary-600 inline-flex items-center hover:underline">
+              Explore
+            </a>
+          </fwb-card>
+        </div>
+        <div class="col-start-5 col-end-8 row-start-2">
+          <fwb-card class="sm:p-10 md:p-12 p-12 w-full" variant="horizontal">
+            <a href="/game">
+              <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                Game Developer
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              I have a strong passion for video game development. I'm fascinated by
+              every aspect of the process, from designing a story or gameplay
+              mechanic to bringing it to life. I enjoy creating environments,
+              developing user interfaces, and programming the interactive dynamics
+              that make each game unique. I’m currently studying the Unity3D engine
+              and enjoy creating Pixel Art in my free time.
+            </p>
+            <a href="/game" class="text-primary-600 inline-flex items-center hover:underline">
+              Explore
+            </a>
+          </fwb-card>
+        </div>
+        <div class="col-start-2 col-end-8 row-start-4 row-end-5 mb-10">
+          <fwb-footer>
+              <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">About me</h5>
+          </fwb-footer>
+        </div>
+      </div>
+    </span>
   </main>
 </template>
