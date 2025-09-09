2baeb4b824de74cb2fcd1b3fa6628dcde0f4d227
stanipetrosyan <stani.petrosyan@gmail.com>
2025-09-09 10:24:46 +0200

    refactor

---
+++ src/components/Tabs.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps(['tabs'])
+const currentTab = ref(Object.keys(props.tabs)[0]);
+
+</script>
+
+<template>
+
+    <aside id="default-sidebar" class="fixed z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+    aria-label="Sidebar">
+
+    <div class="h-full px-3 py-4 overflow-y-auto bg-teal-600 dark:bg-gray-900"> 
+      <ul v-for="(_, tab) in tabs" class="space-y-2 font-medium">
+        <li class="mt-2">
+          <div @click="currentTab = tab" :class="{ 'dark:bg-teal-600 bg-gray-900': currentTab === tab }"
+            class="flex items-center p-2 rounded-lg text-white hover:bg-gray-900 dark:hover:bg-teal-600">
+            <span class="ms-3"> {{ tab }}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </aside>
+
+  <div class="p-4 sm:ml-64">
+    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+      <component :is="tabs[currentTab]"></component>
+    </div>
+  </div>
+
+</template>
